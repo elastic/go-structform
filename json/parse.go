41308dd3ec1e3986b3c35e3af94af94d72833f2c
urso <steffen.siering@elastic.co>
2018-03-02 11:08:59 -0800

    json int parsing

--- json/parse.go
+++ json/parse.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"io"
+	"math"
 	"strconv"
 	"unicode"
 	"unicode/utf16"
@@ -28,9 +29,11 @@ type Parser struct {
 
 	literalBuffer  []byte
 	literalBuffer0 [64]byte
-	isDouble       bool
-	inEscape       bool
-	required       int
+
+	inEscape bool
+	isDouble bool
+
+	required int
 }
 
 var (
@@ -304,6 +307,8 @@ func (p *Parser) stepValue(b []byte, retState state) ([]byte, bool, error) {
 
 	default:
 		// parse number?
+		p.isDouble = false
+
 		isNumber := c == '-' || c == '+' || c == '.' || isDigit(c)
 		if !isNumber {
 			return b, false, errUnknownChar
@@ -444,9 +449,10 @@ func (p *Parser) doString(b []byte) ([]byte, bool, bool, []byte, error) {
 		buf = b[1:]
 	}
 
+	inEscape := p.inEscape
 	for i, c := range buf {
-		if p.inEscape {
-			p.inEscape = false
+		if inEscape {
+			inEscape = false
 			continue
 		}
 
@@ -454,11 +460,11 @@ func (p *Parser) doString(b []byte) ([]byte, bool, bool, []byte, error) {
 			done = true
 			stop = i + delta
 			break
-		}
-		if c == '\\' {
-			p.inEscape = true
+		} else if c == '\\' {
+			inEscape = true
 		}
 	}
+	p.inEscape = inEscape
 
 	if !done {
 		p.literalBuffer = append(p.literalBuffer, b...)
@@ -672,12 +678,57 @@ func (p *Parser) reportNumber(b []byte, isDouble bool) error {
 		}
 	} else {
 		var i int64
-		if i, err = strconv.ParseInt(bytes2Str(b), 10, 64); err == nil {
+		if i, err = parseInt(b); err == nil {
 			err = p.visitor.OnInt64(i)
 		}
 	}
 
 	return err
+}
+
+func parseInt(b []byte) (int64, error) {
+	neg := false
+	if b[0] == '+' {
+		b = b[1:]
+	} else if b[0] == '-' {
+		neg = true
+		b = b[1:]
+	}
+
+	u, err := parseUint(b)
+	n := int64(u)
+	if neg {
+		n = -n
+	}
+	return n, err
+}
+
+func parseUint(b []byte) (uint64, error) {
+	const cutoff = math.MaxUint64/10 + 1
+	const maxVal = math.MaxUint64
+
+	var n uint64
+
+	for _, c := range b {
+		d := int(c) - '0'
+		if d < 0 || d > 9 {
+			panic("syntax error")
+		}
+
+		if n >= cutoff {
+			panic("range error")
+		}
+
+		n *= 10
+		n1 := n + uint64(d)
+		if n1 < n || n1 > maxVal {
+			panic("range error")
+		}
+
+		n = n1
+	}
+
+	return n, nil
 }
 
 func (p *Parser) stepNULL(b []byte) ([]byte, bool, error) {
